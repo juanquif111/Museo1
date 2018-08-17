f41e43516595e8268eb00ed439e2cb842c8aea50
daniel <dapinillav@gmail.com>
2018-08-17 12:18:01 -0500

    CALCULADORA DE SUMA RESTA MULTIPLICACION

--- src/pages/hemeroteca/hemeroteca.ts
+++ src/pages/hemeroteca/hemeroteca.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the HemerotecaPage page.
@@ -10,16 +10,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-hemeroteca',
-  templateUrl: 'hemeroteca.html',
+    selector: 'page-hemeroteca',
+    templateUrl: 'hemeroteca.html',
 })
 export class HemerotecaPage {
+    numero1;
+    numero2;
+    resultado1;
+    resultado2;
+    resultado3;
+    resultado4;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HemerotecaPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad HemerotecaPage');
+    }
+    suma() {
+        this.resultado1 = "La suma de" + this.numero1 + "+" +
+            this.numero2 + "=" + (parseFloat(this.numero1) + parseFloat(this.numero2));
+    }
+    resta() {
+        this.resultado2 = "La resta de" + this.numero1 + "-" +
+            this.numero2 + "=" + (parseFloat(this.numero1) - parseFloat(this.numero2));
+    }
+    multiplica() {
+        this.resultado3 = "La multiplicación de" + this.numero1 + "x" +
+            this.numero2 + "=" + (parseFloat(this.numero1) * parseFloat(this.numero2));
+    }
 
 }
